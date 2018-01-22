290240bfed47dfe45e6af5726a0a1b0243d5a301
Burak Şahin <suspicioususer>
2018-01-22 18:34:29 +0300

    Reset

--- Turkish Flag/flag.js
+++ Turkish Flag/flag.js
@@ -1,10 +1,8 @@
 /***********************
-* SuspiciousUser       *
+* Burak Şahin          *
 * Created : 11.02.2017 *
 * Updated : --.--.---- *
 ***********************/
-
-//Please leave a comment for bugs. I will fix it.
 
 function flag() {
     var g = 480;
@@ -49,51 +47,43 @@ function flag() {
     context.strokeStyle = "rgb(227, 10, 23)";
     context.stroke();
     
-   /*context.beginPath();
+    /*
+    context.beginPath();
     context.arc(m + a + c - innerRadius + e + starRadius, g / 2, starRadius, 0 * Math.PI, 2 * Math.PI);
     context.fillStyle = "white";
     context.fill();
     context.lineWidth = 0;
     context.strokeStyle = "white";
-    context.stroke();*/
+    context.stroke();
+	*/
     
-    /*context2.beginPath();
-    context2.translate(m + a + c - innerRadius + e + starRadius, g / 2);
-
-// initial offset rotation so our star is straight
-    context2.rotate((Math.PI * 4 / 10));
-
-// make a point, 5 times
-    for (var i = 5; i--;) {
-        // draw line up
-        context2.lineTo(0, starRadius);
-        // move origin to current same location as pen
-        context2.translate(0, starRadius);
-        // rotate the drawing board
-        context2.rotate((Math.PI * 2 / 10));
-        // draw line down
-        context2.lineTo(0, -starRadius);
-        // again, move origin to pen...
-        context2.translate(0, -starRadius);
-        // ...and rotate, ready for next arm
-        context2.rotate(-(Math.PI * 6 / 10));
+    /*
+	context2.beginPath();
+    context2.translate(m + a + c - innerRadius + e + starRadius, g / 2);    
+    context2.rotate((Math.PI * 4 / 10)); //initial offset rotation so our star is straight
+    for (var i = 5; i--;) { // make a point, 5 times
+        context2.lineTo(0, starRadius); // draw line up
+        context2.translate(0, starRadius); // move origin to current same location as pen
+        context2.rotate((Math.PI * 2 / 10)); // rotate the drawing board
+        context2.lineTo(0, -starRadius); // draw line down
+        context2.translate(0, -starRadius); // again, move origin to pen...
+        context2.rotate(-(Math.PI * 6 / 10)); // ...and rotate, ready for next arm
     }
-    // last line to connect things up
-    context2.lineTo(0, starRadius);
+    context2.lineTo(0, starRadius); // last line to connect things up
     context2.closePath();
 	context2.fillStyle = "white";
     context2.fill();
-    // stroke the path, you could also .fill()
-	context2.strokeStyle = "white";
-    context2.stroke();*/
+	context2.strokeStyle = "white"; // stroke the path, you could also .fill()
+    context2.stroke();
+	*/
     
     context2.beginPath();
     //context2.moveTo(m + a + c - innerRadius + e + starRadius, g / 2);
-//    context2.lineTo(351.406, 240.963);
-//    context2.lineTo(460.382, 276.372);
-//   context2.lineTo(393.030, 183.671);
-//    context2.lineTo(393.030, 298.256);
-//    context2.lineTo(460.382, 205.554);
+    //context2.lineTo(351.406, 240.963);
+    //context2.lineTo(460.382, 276.372);
+    //context2.lineTo(393.030, 183.671);
+    //context2.lineTo(393.030, 298.256);
+    //context2.lineTo(460.382, 205.554);
 	context2.lineTo(g / 1.3659, g / 1.992);
 	context2.lineTo(g / 1.0426, g / 1.7367);
 	context2.lineTo(g / 1.2212, g / 2.6133);
@@ -102,5 +92,4 @@ function flag() {
     context2.closePath();
     context2.fillStyle = "white";
     context2.fill();
-    
 }
